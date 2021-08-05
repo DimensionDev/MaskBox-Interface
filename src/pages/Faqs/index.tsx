0b4c0825d8725c6b7f850928f88d8b94144a34aa
unclebill <billbill290@gmail.com>
2021-08-05 11:16:05 +0800

    feat: add faqs page

--- src/pages/Faqs/index.tsx
+++ src/pages/Faqs/index.tsx
@@ -1,10 +1,13 @@
+import { ArticleSection } from '@/components';
 import React, { FC } from 'react';
 import styles from './index.module.less';
 
 export const Faqs: FC = () => {
   return (
-    <div>
-      <h1>faqs</h1>
-    </div>
+    <article className={styles.article}>
+      <ArticleSection title="Introducing NFTBOX Marketplace">To do</ArticleSection>
+      <ArticleSection title="How To Get Started with NFTBOX Marketplace">To do</ArticleSection>
+      <ArticleSection title="How To Buy an NFT on NFTBOX">To do</ArticleSection>
+    </article>
   );
 };
