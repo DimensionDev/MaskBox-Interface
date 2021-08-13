4f3a3a52597517f41a5af947060c5a8131ba9a05
unclebill <billbill290@gmail.com>
2021-08-13 14:37:34 +0800

    feat: add open box animation

--- src/page-components/MysteryBox/index.tsx
+++ src/page-components/MysteryBox/index.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
+import { ThickButton } from '@/components';
 
 import styles from './index.module.less';
-import { ThickButton } from '@/components';
 
 interface Props {
   value: string;
@@ -10,16 +10,37 @@ interface Props {
 }
 
 export const MysteryBox: FC<Props> = ({ value, onOpen }) => {
+  const [open, setOpen] = useState(false);
+  const paintingRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!paintingRef.current) {
+      return;
+    }
+    const animationendFn = () => {
+      setTimeout(() => {
+        onOpen?.();
+      }, 150);
+    };
+    paintingRef.current?.addEventListener('animationend', animationendFn);
+    return () => {
+      paintingRef.current?.removeEventListener('animationend', animationendFn);
+    };
+  }, [paintingRef.current, onOpen]);
   return (
     <div className={styles.container}>
       <div className={styles.boxGroup}>
         <div className={classnames(styles.comp, styles.lisa)} />
         <div className={classnames(styles.comp, styles.sunflower)} />
         <div className={classnames(styles.comp, styles.box)} />
+        <div
+          ref={paintingRef}
+          className={classnames(styles.comp, styles.painting, open ? styles.open : null)}
+        />
       </div>
       <div className={styles.buttonGroup}>
         <p className={styles.value}>{value}</p>
-        <ThickButton className={styles.button} onClick={onOpen}>
+        <ThickButton className={styles.button} onClick={() => setOpen(true)}>
           Open Red Packet
         </ThickButton>
       </div>
