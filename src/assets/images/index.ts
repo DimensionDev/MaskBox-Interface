2ff2ddcc7ac7c3908956b4897411753cfddc740b
unclebill <billbill290@gmail.com>
2021-10-01 13:15:49 +0800

    fix: new profile background image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,3 @@
 export const avatarImage = new URL('./avatar.png', import.meta.url).href;
 
 export const logoImage = new URL('./logo.svg', import.meta.url).href;
-
-export const profilebgImage = new URL('./profile-bg.png', import.meta.url).href;
