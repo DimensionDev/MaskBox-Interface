bef9e01312f1526afac4673b70c353b229b08f66
unclebill <billbill290@gmail.com>
2021-12-03 11:38:40 +0800

    fix: set up mail list configuration

--- src/components/NewsletterBox/index.tsx
+++ src/components/NewsletterBox/index.tsx
@@ -11,10 +11,16 @@ export const NewsletterBox: FC<Props> = ({ className, ...rest }) => {
   return (
     <div className={classnames(styles.newsletter, className)} {...rest}>
       <div className={styles.newsletterTitle}>{t('Join Newsletter')}</div>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        method="post"
+        target="_blank"
+        action="https://dimension.us20.list-manage.com/subscribe/post?u=7c1e7d9e1839f1c23061ade74&id=7c6708a2c7"
+      >
         <input
           className={styles.input}
           type="email"
+          name="EMAIL"
           placeholder={t('Enter your email') as string}
         />
         <Button className={styles.button} size="small" colorScheme="primary" circle>
