17167db0f1847973c5c1503ec656d441f4a48443
unclebill <billbill290@gmail.com>
2022-01-05 17:31:52 +0800

    fix: show all data in dev mode

--- src/hooks/useIgnoreBoxes.ts
+++ src/hooks/useIgnoreBoxes.ts
@@ -1,16 +1,18 @@
 import { useWeb3Context } from '@/contexts';
-import { getIgnoreIds, getSkips } from '@/lib';
+import { DEV_MODE_ENABLED, getIgnoreIds, getSkips } from '@/lib';
 import { EMPTY_LIST } from '@/utils';
 import { useMemo } from 'react';
-
+const NO_IGNORE = { skips: 0, ignoreIds: ['0'], total: 0 };
 export function useIgnoreBoxes() {
   const { providerChainId: chainId } = useWeb3Context();
   const skips = useMemo(() => (chainId ? getSkips(chainId) : 0), [chainId]);
   const ignoreIds = useMemo(() => (chainId ? getIgnoreIds(chainId) : EMPTY_LIST), [chainId]);
   const total = skips + ignoreIds.length;
-  return {
-    skips,
-    ignoreIds,
-    total,
-  };
+  return DEV_MODE_ENABLED
+    ? NO_IGNORE
+    : {
+        skips,
+        ignoreIds,
+        total,
+      };
 }
